863493e22be245117de96e8de388cf67a4969251
NguyenTruongToan2606 <redbeaver26062002@gmail.com>
2025-07-24 14:52:22 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -21,6 +21,13 @@ const routes = [
         props: true // Truyền các biến trong $route.params vào làm props
     },
 
+    {
+        path: "/contacts/add",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+    },
+
+
 ];
     const router = createRouter({
         history: createWebHistory(import.meta.env.BASE_URL),
